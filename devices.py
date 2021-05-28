566c90d85ab09540fa2968a0ab2fe68c1bed9da3
RootLess <eygwork@gmail.com>
2021-05-28 21:08:14 +0300

    Add JWT requirement Acquire and release

--- devices.py
+++ devices.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 from flask import abort, request
+from flask_jwt_simple import jwt_required
 
 devices = {'1': 1, '2': 2, '3': 3}
 
@@ -12,9 +13,13 @@ class Device(Resource):
         self._check_if_device_exists(device_id)
         return self.devices[device_id]
 
+    @jwt_required
     def post(self, device_id):
         self._check_if_device_exists(device_id)
-        data = request.json
+        if not request.is_json:
+            return 'Missing request json', 400
+
+        data = request.get_json()
         action = data.get('action', None)
         if not action:
             return 'Missing action parameter', 400
