fe2c7d1a4f3d6bb9c3c7894d659c154caa2de8bd
RootLess <eygwork@gmail.com>
2021-05-29 01:02:23 +0300

    Use DB instead of place holder dict

--- device_resources.py
+++ device_resources.py
@@ -1,17 +1,16 @@
 from flask_restful import Resource
 from flask import abort, request
 from flask_jwt_simple import jwt_required
-
-devices = {'1': 1, '2': 2, '3': 3}
+from db import DeviceDB
 
 
 class Device(Resource):
-    def __init__(self):
-        self.devices = devices
+    def __init__(self, db=DeviceDB()):
+        self.db = db
 
     def get(self, device_id):
         self._check_if_device_exists(device_id)
-        return self.devices[device_id]
+        return self.db.get_device(device_id).json
 
     @jwt_required
     def post(self, device_id):
@@ -30,19 +29,19 @@ class Device(Resource):
             return self._release(device_id)
 
     def _check_if_device_exists(self, device_id):
-        if device_id not in self.devices:
+        if not self.db.get_device(device_id):
             abort(400, f'Device: {device_id} does not exist')
 
     def _acquire(self, device_id):
-        return 'acquire'
+        raise NotImplemented
 
     def _release(self, device_id):
-        return 'release'
+        raise NotImplemented
 
 
 class DeviceList(Resource):
-    def __init__(self):
-        self.devices = devices
+    def __init__(self, db=DeviceDB()):
+        self.db = db
 
     def get(self):
-        return self.devices
+        return [device.json for device in self.db.get_all_devices()]
